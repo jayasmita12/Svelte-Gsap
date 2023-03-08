b07262af26200aa8001275419f6991bd358c3342
Jayasmita Sahu <96156277+jayasmita12@users.noreply.github.com>
2023-03-08 08:36:01 +0530

    second commit

--- .netlify/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
-export const file = '_app/immutable/entry/_layout.svelte.b50dfe83.mjs';
-export const imports = ["_app/immutable/entry/_layout.svelte.b50dfe83.mjs","_app/immutable/chunks/index.1757783d.mjs"];
-export const stylesheets = ["_app/immutable/assets/_layout.de300067.css"];
-export const fonts = [];
--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/entry/error.svelte.b8970ee4.mjs';
-export const imports = ["_app/immutable/entry/error.svelte.b8970ee4.mjs","_app/immutable/chunks/index.1757783d.mjs","_app/immutable/chunks/singletons.8c3b52c6.mjs"];
-export const stylesheets = [];
-export const fonts = [];
