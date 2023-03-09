20fa622685ab94b4f0046722fc5f779fed742db6
Jayasmita Sahu <96156277+jayasmita12@users.noreply.github.com>
2023-03-09 20:38:13 +0530

    added scroll smoother,scroll trigger, splitText

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,51 @@ import Flip from "../lib/components/flip/Flip.svelte";
 	import Flip3 from "../lib/components/flip/Flip3.svelte";
 	import MotionPath from "../lib/components/motionPath/MotionPath.svelte";
 	import Observer from "../lib/components/observer/Observer.svelte";
+	import SplitText from "../lib/components/textsplit/SplitText.svelte";
+	import gsap from "gsap";
+	import {ScrollTrigger} from "$lib/script/gsap/ScrollTrigger";
+	import {ScrollSmoother} from "$lib/script/gsap/ScrollSmoother";
+	import {onMount} from "svelte"
+	import TypeWritter from "../lib/components/typewritter/TypeWritter.svelte";
+	
+	onMount(()=>{
+		gsap.registerPlugin(ScrollSmoother);
+		ScrollSmoother.create({
+			wrapper:"#smooth-wrapper",
+			content:"#smooth-content",
+			smooth:1,
+			effects: true,  
+			smoothTouch: 0.1,  
+		})
+	})
 
 </script>
 <!-- <Flip/> -->
 <!-- <Flip2/> -->
-<Flip3/>
-<MotionPath/>
-<Dragable/>
-<Observer/>
+<div id="smooth-wrapper">
+	<div id="smooth-content">
+		<div>
+			<Flip3/>
+		</div>
+		<div>
+			<MotionPath/>
+		</div>
+		<div>
+			<Dragable/>
+		</div>
+		<div>
+			<Observer/>
+		</div>
+		<div>
+			<SplitText/>
+		</div>
+		<div>
+			<TypeWritter/>
+		</div>
+	</div>
+</div>
+
+
+
+
+
