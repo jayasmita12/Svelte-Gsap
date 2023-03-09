b8756c0c861b6e85d79fa7e5f9f55378b042f18c
Jayasmita Sahu <96156277+jayasmita12@users.noreply.github.com>
2023-03-09 20:42:22 +0530

    added scroll smoother,scroll trigger, splitText changing

--- src/lib/components/textsplit/SplitText.svelte
+++ src/lib/components/textsplit/SplitText.svelte
@@ -9,55 +9,49 @@
 		spliting();
 		// scroll()
 		
-		gsap.from(".title1",{
-			opacity:1,
-			ScrollTrigger:{
+		// gsap.from(".title1",{
+		// 	opacity:1,
+		// 	ScrollTrigger:{
 				
-				trigger:".title1",
-				opacity:0,
-				start:"bottom bottom",
-				end:"top top",
-				scrub:true
-			}
-		})
+		// 		trigger:".title1",
+		// 		opacity:0,
+		// 		start:"bottom bottom",
+		// 		end:"top top",
+		// 		scrub:true
+		// 	}
+		// })
 	});
 
-	const spliting=()=>{
-		let splittext = new SplitText(".title1",{
-			type:"words"
-		})
-	}
+	var tl;
+	const spliting = () => {
+		tl = gsap.timeline();
+		var mySplitText1 = new SplitText('#title1', { type: 'words,chars' });
+		var chars1 = mySplitText1.chars;
+		gsap.set(chars1, { perspective: 400 });
+		tl.from(chars1, {
+			duration: 0.8,
+			opacity: 0,
+			scale: 0,
+			y: 120,
+			rotationX: 180,
+			transformOrigin: '0% 50% -50',
+			ease: 'back',
+			stagger: 0.01
+		});
 
-	// var tl;
-	// const spliting = () => {
-	// 	tl = gsap.timeline();
-	// 	var mySplitText1 = new SplitText('#title1', { type: 'words,chars' });
-	// 	var chars1 = mySplitText1.chars;
-		
-	// 	tl.from(chars1, {
-	// 		duration: 0.8,
-	// 		opacity: 0,
-	// 		scale: 0,
-	// 		y: 120,
-	// 		rotationX: 180,
-	// 		transformOrigin: '0% 50% -50',
-	// 		ease: 'back',
-	// 		stagger: 0.01
-	// 	});
-
-	// 	var tl1 = gsap.timeline();
-	// 	var mySplitText2 = new SplitText('#title2', { type: 'lines' });
-	// 	var lines2 = mySplitText2.lines;
-	// 	gsap.set('#title2', { perspective: 400 });
-	// 	tl1.from(lines2, {
-	// 		duration: 0.5,
-	// 		opacity: 0,
-	// 		rotationX: -120,
-	// 		force3D: true,
-	// 		transformOrigin: 'top center -150',
-	// 		stagger: 0.1
-	// 	});
-	// };
+		var tl1 = gsap.timeline();
+		var mySplitText2 = new SplitText('#title2', { type: 'lines' });
+		var lines2 = mySplitText2.lines;
+		gsap.set(lines2, { perspective: 400 });
+		tl1.from(lines2, {
+			duration: 0.5,
+			opacity: 0,
+			rotationX: -120,
+			force3D: true,
+			transformOrigin: 'top center -150',
+			stagger: 0.1
+		});
+	};
 
 	
 </script>
@@ -65,7 +59,7 @@
 	<div>
 		<div class="max-w-full h-screen px-20 flex flex-col space-y-5 justify-center items-center bg-slate-500 text-6xl font-bold">
 		<div class="box1">
-			<div class="title1 border-2">
+			<div id="title1">
 				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit similique corrupti quasi
 				nesciunt? Doloremque, illo maiores. Hic enim possimus sit itaque explicabo et magni natus. Ab
 				asperiores sit quidem minima.
